a7cf17dd653727eb6e881eb994b8a3c9f9d558bb
Aghata Sophia <aghatasophia@gmail.com>
2024-04-04 20:52:06 -0400

    Add props, article element and styles to the Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,37 @@
-export default function Card() {
+import { ReactElement } from 'react';
+import './Card.css';
+
+/**
+ * @param {{ 
+ *  image: { 
+ *      url: string,
+ *      width?: number,
+ *      height?: number
+ *  },
+ *  title: string,
+ *  subtitle: string,
+ *  align?: string,
+ * }} props - Image width and height must be a number in 'em' css unit 
+ * @returns {ReactElement} article element with a card class
+ */
+export default function Card(props) {
+    const { image, title, subtitle, align = 'center' } = props;
+
+    const imageStyle = {
+        width: `${image.width ?? 10}em`,
+        height: `${image.height ?? 10}em`,
+    };
+
     return (
-        <div>Card</div>
+        <article className="card">
+            <header className='card-header'>
+                <img src={image.url} style={imageStyle} />
+            </header>
+
+            <div className='card-body' style={{ textAlign: align }}>
+                <h5 className="title">{title}</h5>
+                <h6 className="subtitle">{subtitle}</h6>
+            </div>
+        </article>
     )
 }
