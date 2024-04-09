c89a6b8f235d3fdd40c8ea68b6ca58f396385e4b
Aghata Sophia <aghatasophia@gmail.com>
2024-04-08 23:32:57 -0400

    Add home component to the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+
+import Home from './components/Home/Home';
 import Menu from './components/Menu/Menu';
+import Navbar from './components/Navbar/Navbar';
 import Team from './components/Team/Team';
 
 
 function App() {
-  return (<>
-  <Menu/>
-  <Team/>
-    
-  </>);
+    return (<>
+        <section className='hero'>
+            <Navbar />
+            <Home />
+        </section>
+
+        <Menu />
+        <Team />
+    </>);
 }
 
 export default App;
