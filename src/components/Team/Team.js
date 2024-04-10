173a895821a6a2b611d4380947e06b414a6e4b05
dianatobe96 <dianana996@gmail.com>
2024-04-10 17:21:25 -0400

    I add the css on the team

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -8,13 +8,15 @@ import chef4 from "../../assets/images/chef4.jpg";
 
 export default function Team() {
     return (
-        <div>
+        <div className="container">
             <h2 className="textteam">Meet our team</h2>
-            
+             
+             <div className="cardteam">
              <Card image={{ url: chef1, width: 15, height: 15 }} title="Bulbul Hassan" subtitle="Chef" align='left' />
              <Card image={{ url: chef2, width: 15, height: 15 }} title="Lia Smith" subtitle="Founder" align='left' />
              <Card image={{ url: chef3, width: 15, height: 15 }} title="Michael Nur" subtitle="Specialist" align='left' />
              <Card image={{ url: chef4, width: 15, height: 15 }} title="Munna Kathy" subtitle="Owner" align='left' />
+             </div>
         </div>
         )
     
