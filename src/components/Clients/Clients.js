8aff4cde0405aef77818c74f4c82b213ca0b393b
dianatobe96 <dianana996@gmail.com>
2024-04-10 17:26:11 -0400

    I add css to the card on clients file

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -9,14 +9,17 @@ import client6 from "../../assets/images/client6.png"
 
 export default function Clients() {
     return (
-        <div>
+        <div className="container">
             <h2 className="textclients">Clients</h2>
-            <Card image={{ url: client1, width: 20, height: 10 }} title="" subtitle="" align='left' />
-            <Card image={{ url: client2, width: 20, height: 15 }} title="" subtitle="" align='left' />
-            <Card image={{ url: client3, width: 17, height: 15 }} title="" subtitle="" align='left' />
-            <Card image={{ url: client4, width: 18, height: 17 }} title="" subtitle="" align='left' />
-            <Card image={{ url: client5, width: 20, height: 15 }} title="" subtitle="" align='left' />
-            <Card image={{ url: client6, width: 20, height: 21 }} title="" subtitle="" align='left' />
+
+                <div className="cardclients">
+                <Card image={{ url: client1, width: 20, height: 10 }} title="" subtitle="" align='left' />
+                <Card image={{ url: client2, width: 20, height: 15 }} title="" subtitle="" align='left' />
+                <Card image={{ url: client3, width: 17, height: 15 }} title="" subtitle="" align='left' />
+                <Card image={{ url: client4, width: 18, height: 17 }} title="" subtitle="" align='left' />
+                <Card image={{ url: client5, width: 20, height: 15 }} title="" subtitle="" align='left' />
+                <Card image={{ url: client6, width: 20, height: 21 }} title="" subtitle="" align='left' />
+                </div>
             </div>
     )
 }
